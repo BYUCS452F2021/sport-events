2dcd4a771e1fc2c72d82ef62669e88d08685ccb2
Victor Velasco <victorvelasco098@gmail.com>
2021-10-14 01:01:29 -0600

    added vuetify and event-panel page

--- sport-events/src/main.js
+++ sport-events/src/main.js
@@ -1,10 +1,18 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ sport-events/src/views/Event_Panel.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="event_panel">
+        <h1>Hello World</h1>
+        <div id="events">
+            <v-btn
+            elevation="2"
+            outlined
+            x-large>
+            victor velasco
+            volleyball
+            Medium
+            Kiwanis Park
+            </v-btn>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'event_panel',
+}
+</script>
+
+<style scoped>
+#events {
+    display: flex;
+    justify-content: center;
+}
+.v-btn {
+    margin: 20px;
+    width: 80%;
+    
+}
+</style>
