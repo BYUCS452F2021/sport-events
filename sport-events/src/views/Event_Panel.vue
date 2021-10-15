b861c957f5e53c20131d6dc2da7066bcc1999ecf
Victor Velasco <victorvelasco098@gmail.com>
2021-10-14 23:11:12 -0600

    created v-for feature

--- sport-events/src/views/Event_Panel.vue
+++ sport-events/src/views/Event_Panel.vue
@@ -1,15 +1,27 @@
 <template>
     <div class="event_panel">
-        <h1>Hello World</h1>
+        <div id="header">
+            <h1>Sport Events</h1>
+            <v-text-field
+            label="Search"
+            outlined
+            dense>
+            </v-text-field>
+        </div>
         <div id="events">
             <v-btn
             elevation="2"
             outlined
-            x-large>
-            victor velasco
-            volleyball
-            Medium
-            Kiwanis Park
+            x-large
+            v-for="event in events"
+            :key="event">
+                <v-row>
+                    <v-col>{{ event.username }}</v-col>
+                    <v-col>{{ event.sport_name }}</v-col>
+                    <v-col>{{ event.skill_level }}</v-col>
+                    <v-col>{{ event.date }}</v-col>
+                    <v-col>{{ event.city }}</v-col>
+                </v-row>
             </v-btn>
         </div>
     </div>
@@ -18,13 +30,48 @@
 <script>
 export default {
   name: 'event_panel',
+  data: function() {
+      return {
+          events: [
+            {
+                username:  "victorvel", 
+                sport_name:  "volleyball", 
+                skill_level:  "medium", 
+                date: "10/31/21 5:00PM", 
+                city: "Provo"
+            }, 
+            {
+                username: "ung123", 
+                sport_name: "basketball", 
+                skill_level: "easy", 
+                date: "10/25/21 4:00PM", 
+                city: "Orem"
+            }, 
+            {
+                username: "Thor", 
+                sport_name: "fighting", 
+                skill_level: "expert", 
+                date: "12/25/21 3:00PM", 
+                city: "Lehi"
+            }
+        ]
+      }
+  }
 }
 </script>
 
 <style scoped>
+#header {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
 #events {
     display: flex;
     justify-content: center;
+    flex-direction: column;
+    align-items: center;
 }
 .v-btn {
     margin: 20px;
