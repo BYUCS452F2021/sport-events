fa2a477720f1c281712d2dacb770113e0eb5829b
Victor Velasco <victorvelasco098@gmail.com>
2021-10-20 12:42:22 -0600

    create login page

---
+++ sport-events/src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="login">
+        <b-card title="Login" style="width: 40%;" id="form-card">
+            <b-form>
+                <b-form-input
+                placeholder="username"
+                class="form-input">
+                </b-form-input>
+                <b-form-input
+                placeholder="password"
+                class="form-input">
+                </b-form-input>
+            </b-form>
+            <b-button
+            variant="primary"
+            id="btn">
+                submit
+            </b-button>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .login {
+        display: flex;
+        justify-content: center;
+    }
+    #form-card {
+        margin-top: 10vw;
+    }
+    .form-input {
+        margin: 20px 0;
+    }
+    #btn {
+        cursor: pointer;
+    }
+</style>
