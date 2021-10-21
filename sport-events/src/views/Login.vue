b50fa4636143cad3447fae5965f89cd25dd95934
Victor Velasco <victorvelasco098@gmail.com>
2021-10-20 23:25:47 -0600

    created login page

--- sport-events/src/views/Login.vue
+++ sport-events/src/views/Login.vue
@@ -1,28 +1,48 @@
 <template>
     <div class="login">
         <b-card title="Login" style="width: 40%;" id="form-card">
-            <b-form>
+            <b-form @submit="onSubmit">
                 <b-form-input
+                id="username"
+                v-model="form.username"
                 placeholder="username"
                 class="form-input">
                 </b-form-input>
                 <b-form-input
+                id="password"
+                type="password"
+                v-model="form.password"
                 placeholder="password"
                 class="form-input">
                 </b-form-input>
+                <b-button
+                variant="primary"
+                id="btn"
+                type="submit">
+                    submit
+                </b-button>
+                <p id="link">Don't have an account? Create one <a href="#">here</a></p>
             </b-form>
-            <b-button
-            variant="primary"
-            id="btn">
-                submit
-            </b-button>
         </b-card>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            form: {
+                username: '', 
+                password: ''
+            }
+        }
+    }, 
+    methods: {
+        onSubmit(event) {
+            event.preventDefault();
+            alert(JSON.stringify(this.form));
+      },
+    }
 }
 </script>
 
@@ -39,5 +59,9 @@ export default {
     }
     #btn {
         cursor: pointer;
+        margin-bottom: 10px;
+    }
+    #link {
+        position: absolute;
     }
 </style>
