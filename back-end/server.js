5a321b6b8c49b83c5ce77fe921fdcc6fb3006865
Jake Reeves <reeves.jake5@gmail.com>
2021-10-27 16:53:24 -0600

    added endpoint to get all members of an event

--- back-end/server.js
+++ back-end/server.js
@@ -38,16 +38,34 @@ let fakeEvents = [
     difficulty: 8,
     playersNeeded: 11
   },
-]
+];
+
+let fakeMembership = [
+  {
+    eventID: 1,
+    userID: 1,
+    approved: true
+  },
+  {
+    eventID: 1,
+    userID: 2,
+    approved: true
+  },
+  {
+    eventID: 1,
+    userID: 3,
+    approved: false
+  }
+];
 
 //register
 app.post('/register', async (req, res) => {
   let user = {
     username: req.body.username,
     password: req.body.password
-  }
+  };
   //todo: put user in the db. if username already exists, send failure message. otherwise, send userID of new user.
-  res.send({userID: 1})
+  res.send({userID: 1});
 });
 
 //login
@@ -55,22 +73,22 @@ app.post('/login', async (req, res) => {
   let credentials = {
     username: req.body.username,
     password: req.body.password
-  }
+  };
   //todo: check to see if credentials exist in the db (and they match). If yes, send userID of user, else send failure.
-  res.send({userID: 1})
+  res.send({userID: 1});
 });
 
 //get upcoming events
 app.get('/upcoming', async (req, res) => {
   //todo: find all events not yet passed, join with user table, return it
-  res.send(fakeEvents)
+  res.send(fakeEvents);
 });
 
 //get events I have joined
 app.get('/joined', async (req, res) => {
   //todo: get events based on event membership table (approved only)
-  let userID = req.body.userID
-  res.send(fakeEvents)
+  let userID = req.body.userID;
+  res.send(fakeEvents);
 });
 
 //create new event
@@ -83,22 +101,22 @@ app.post('/event', async (req, res) => {
     dateTime: req.body.dateTime,
     difficulty: req.body.difficulty,
     playersNeeded: req.body.playersNeeded
-  }
-  res.send()
+  };
+  res.send();
 });
 
 //edit event
 app.put('/event/:id', async (req, res) => {
   //todo: find event with eventID, update it.
-  let eventID = req.params.id
+  let eventID = req.params.id;
   let updatedEvent = {
     sport: req.body.sport,
     city: req.body.city,
     dateTime: req.body.dateTime,
     difficulty: req.body.difficulty,
     playersNeeded: req.body.playersNeeded
-  }
-  res.send()
+  };
+  res.send();
 });
 
 //request to join event
@@ -107,17 +125,24 @@ app.post('/membership', async (req, res) => {
     eventID: req.body.eventID,
     userID: req.body.userID,
     approved: false
-  }
-  res.send()
+  };
+  res.send();
 });
 
 //manage event membership
 app.put('/membership', async (req, res) => {
-  let eventID = req.body.eventID
-  let userID = req.body.userID
-  let approved = req.body.approved
+  let eventID = req.body.eventID;
+  let userID = req.body.userID;
+  let approved = req.body.approved;
   //todo: update the membership with the new approval status
-  res.send()
+  res.send();
+});
+
+//get all members (whether approved or not) of an event
+app.get('/membership/:id', async (req, res) => {
+  let eventID = req.params.id;
+  //todo: get all records from membership table with that eventID.
+  res.send(fakeMembership);
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
