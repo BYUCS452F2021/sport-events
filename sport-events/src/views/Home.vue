9ca78c5b207d16e9e1dbdc8ce7bed971256afe75
Parker Alvin Nichols <nicholsp@rhodeisland.cs.byu.edu>
2021-10-28 22:20:11 -0600

    Joined events front end

--- sport-events/src/views/Home.vue
+++ sport-events/src/views/Home.vue
@@ -6,7 +6,16 @@
       <h2>Upcoming Events</h2>
       <br>
       <b-form-input v-model="text" placeholder="Search"></b-form-input>
-      <b-table striped hover :items="items"></b-table>
+      <b-table striped hover :items="items" :fields="fields">
+        <template #cell(add)="row">
+          <b-button v-if="isEventJoinedcom(row)" @click="addToJoined(row)">
+            Add Event
+          </b-button>
+          <b-button v-else @click="removeFromJoined(row)">
+            Remove Event
+          </b-button>
+        </template>
+      </b-table>
     </div>
   </div>
 </template>
@@ -25,29 +34,91 @@ export default {
   data() {
       return {
         items: [],
-        fields: ["Creator Username", "Sport", "City", "Date Time", "Difficulty", "Players Needed"],
+        fields: ["creator", "sport", "city", "dateTime", "difficulty", "playersNeeded",
+        { key: 'add', label: 'Add' }],
         text: "",
+        eventsJoined: [],
       }
     },
-  async created() {
-    try {
-      let response = await axios.get("/upcoming");
-      for (let event of response.data) {
-        let eventDate = new Date(event.dateTime * 1000);
-        event.dateTime = moment(eventDate).format('MMMM Do YYYY, h:mm:ss a');
-        this.items.push(event);
-      }
-
-    } catch(error) {
-      console.log(error);
-    }
-  },
   computed: {
     not_logged_in() {
       if (this.$root.$data.userID) {
         return false;
       }
       return true;
+    },
+    isEventJoinedcom(event) {
+      return event.isJoined;
+    }
+  },
+  methods: {
+    async addtoJoined(event) {
+      let userID = this.$root.$data.userID;
+      try {
+        await axios.post("/membership", {
+          userID: userID,
+          eventID: event.eventID
+        })
+        this.items[event.index].isJoined = true;
+        this.$forceUpdate()
+      } catch(error) {
+        console.log(error)
+      }
+    },
+    async removeFromJoined(event) {
+      let userID = this.$root.$data.userID;
+      try {
+        await axios.delete("/membership", {
+          userID: userID,
+          eventID: event.eventID
+        });
+        console.log(event);
+        this.items[event.index].isJoined = false;
+        this.$forceUpdate()
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    isEventJoined(id) {
+      for (let eventID of this.eventsJoined) {
+        if (eventID == id) {
+          return true;
+        }
+      }
+      return false;
+    }
+  },
+  async created() {
+    try {
+      let response = await axios.get("/joined", {
+        userID: this.$root.$data.userID,
+      });
+
+      for (let event of response.data) {
+        this.eventsJoined.push(event.eventID);
+      }
+
+      response = await axios.get("/upcoming");
+      for (let event of response.data) {
+        let eventDate = new Date(event.dateTime * 1000);
+        event.dateTime = moment(eventDate).format('MMMM Do YYYY, h:mm a');
+
+        if (this.isEventJoined(event.eventID)) {
+          event.isJoined = true;
+        }
+        else {
+          event.isJoined = false;
+        }
+
+        event.index = this.items.length
+        this.items.push(event);
+      }
+
+
+
+
+    } catch(error) {
+      console.log(error);
     }
   },
 }
--- sport-events/src/views/Joined.vue
+++ sport-events/src/views/Joined.vue
@@ -3,30 +3,62 @@
     <br>
     <h2>Joined Events</h2>
     <br>
-    <b-table striped hover :items="items"></b-table>
+    <b-table striped hover :items="items" :fields="fields">
+      <template #cell(remove)="row">
+        <b-button @click="removeFromJoined(row)">
+          Remove Event
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
-
+import axios from "axios"
+import moment from "moment"
 export default {
   name: 'Home',
   components: {
   },
-  created() {
+  async created() {
     if (!this.$root.$data.userID) {
       this.$router.push("/")
+    }
+
+    let response = await axios.get("/joined", {
+      userID: this.$root.$data.userID,
+    });
+
+    for (let event of response.data) {
+      let eventDate = new Date(event.dateTime * 1000);
+      event.dateTime = moment(eventDate).format('MMMM Do YYYY, h:mm a');
+      event.index = this.items.length;
+      this.items.push(event);
     }
   },
   data() {
       return {
-        items: [
-          { sport_name: '', city: '', date: '', time: '', difficulty_level: '', players_needed: '' },
-
-        ]
+      items: [],
+      fields: ["creator", "sport", "city", "dateTime", "difficulty", "playersNeeded",
+      { key: 'remove', label: 'Remove' }],
       }
-    }
+    },
+  methods: {
+    async removeFromJoined(event) {
+      let userID = this.$root.$data.userID;
+      try {
+        await axios.delete("/membership", {
+          userID: userID,
+          eventID: event.eventID
+        });
+        console.log(event);
+        this.items.splice(event.index, 1);
+        this.$forceUpdate()
+      } catch(error) {
+        console.log(error);
+      }
+    },
+  }
 }
 </script>
